b4e2c12e8b9b50adff93ff8215340d81d9e100ed
Jose Ignacio Vaqueiro Gonzalez <jignaciovg@hotmail.com>
2021-05-08 01:33:12 -0500

    Produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_SETTINGS: {
+    apiKey: "AIzaSyC0Caf76F9v-GR-nbX_8agMDT_Bc5ZCRpg",
+    authDomain: "oauth2-b0c08.firebaseapp.com",
+    projectId: "oauth2-b0c08",
+    storageBucket: "oauth2-b0c08.appspot.com",
+    messagingSenderId: "473905630948",
+    appId: "1:473905630948:web:861e97423a9796a829a6f1",
+    measurementId: "G-TKZLE9FV25"
+  },
+  API_KEY: '6091de1ee195a3a7ab254ea2',
+  API: {
+    EndPoint: {
+      Node:'http://localhost:3003/'
+    }
+  }
 };
